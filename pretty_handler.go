39cb2c501eb55d8a5d33f1909c413bf41740143e
Vitalii Furmanov <furmanov.vitaliy@gmail.com>
2024-11-01 13:38:02 +0100

    fix: add pretty handler

--- pretty_handler.go
+++ pretty_handler.go
@@ -1,12 +1,14 @@
 package logger
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log/slog"
 	"runtime"
+	"strings"
 	"time"
 
 	stdLog "log"
@@ -17,8 +19,9 @@ import (
 type PrettyHandler struct {
 	opts *slog.HandlerOptions
 	slog.Handler
-	l     *stdLog.Logger
-	attrs []slog.Attr
+	l      *stdLog.Logger
+	attrs  []slog.Attr
+	groups []string
 }
 
 func NewPrettyHandler(w io.Writer, opts *slog.HandlerOptions) *PrettyHandler {
@@ -46,18 +49,13 @@ func (h *PrettyHandler) Handle(_ context.Context, r slog.Record) error {
 }
 
 func (h *PrettyHandler) colorizeLevel(level slog.Level) string {
-	switch level {
-	case slog.LevelDebug:
-		return color.MagentaString(level.String() + ":")
-	case slog.LevelInfo:
-		return color.BlueString(level.String() + ":")
-	case slog.LevelWarn:
-		return color.YellowString(level.String() + ":")
-	case slog.LevelError:
-		return color.RedString(level.String() + ":")
-	default:
-		return level.String() + ":"
+	colorMap := map[slog.Level]string{
+		slog.LevelDebug: color.HiMagentaString(level.String() + ":"),
+		slog.LevelInfo:  color.HiBlueString(level.String() + ":"),
+		slog.LevelWarn:  color.HiYellowString(level.String() + ":"),
+		slog.LevelError: color.HiRedString(level.String() + ":"),
 	}
+	return colorMap[level] // Default case returns white
 }
 
 func (h *PrettyHandler) getSourceInfo(r slog.Record) (string, int) {
@@ -70,13 +68,15 @@ func (h *PrettyHandler) getSourceInfo(r slog.Record) (string, int) {
 }
 
 func (h *PrettyHandler) getFormattedFields(r slog.Record) (string, error) {
-	fields := make(map[string]interface{}, r.NumAttrs())
+	fields := make(map[string]interface{})
+
 	r.Attrs(func(a slog.Attr) bool {
-		fields[a.Key] = a.Value.Any()
+		fields[a.Key] = extractValue(a.Value)
 		return true
 	})
+
 	for _, a := range h.attrs {
-		fields[a.Key] = a.Value.Any()
+		fields[a.Key] = extractValue(a.Value)
 	}
 
 	if len(fields) == 0 {
@@ -86,30 +86,98 @@ func (h *PrettyHandler) getFormattedFields(r slog.Record) (string, error) {
 	return string(b), err
 }
 
+func extractValue(val slog.Value) interface{} {
+	switch v := val.Any().(type) {
+	case string, int, int64, float64, bool:
+		return v
+	case fmt.Stringer:
+		return v.String()
+	case []slog.Attr:
+		return extractSubAttrs(v)
+	default:
+		return val.Any()
+	}
+}
+
+func extractSubAttrs(attrs []slog.Attr) map[string]interface{} {
+	mapValue := make(map[string]interface{})
+	for _, subAttr := range attrs {
+		mapValue[subAttr.Key] = extractValue(subAttr.Value)
+	}
+	return mapValue
+}
+
 func (h *PrettyHandler) formatTime(t time.Time) string {
-	return "[" + t.Format(time.Kitchen) + "]"
+	return "┌╴[" + t.Format(time.Kitchen) + "]"
+}
+
+func colorizeJson(data map[string]interface{}, level int) string {
+	var buffer bytes.Buffer
+	colorFunc := getColorFunc(level)
+
+	indent := "   "
+	for key, value := range data {
+		buffer.WriteString(fmt.Sprintf("%s%s", repeat(indent, level), colorFunc(key)))
+		if nestedMap, ok := value.(map[string]interface{}); ok {
+			buffer.WriteString("{\n")
+			buffer.WriteString(colorizeJson(nestedMap, level+1))
+			buffer.WriteString(fmt.Sprintf("%s}\n", repeat(indent, level)))
+		} else {
+			valStr, _ := json.Marshal(value)
+			buffer.WriteString(color.HiWhiteString(":%s\n", valStr))
+		}
+	}
+	return buffer.String()
+}
+
+func getColorFunc(level int) func(string, ...interface{}) string {
+	switch level {
+	case 0:
+		return color.HiMagentaString
+	case 1:
+		return color.HiRedString
+	case 2:
+		return color.HiYellowString
+	case 3:
+		return color.HiGreenString
+	case 4:
+		return color.HiBlueString
+	case 5:
+		return color.HiCyanString
+	default:
+		return color.WhiteString
+	}
+}
+
+func repeat(s string, count int) string {
+	return fmt.Sprintf("│%s", strings.Repeat(s, count))
 }
 
 func (h *PrettyHandler) printLog(timeStr, level, msg, fields, file string, line int) {
-	if file != "" {
-		h.l.Println(
-			timeStr,
-			level,
-			msg,
-			color.WhiteString(fields),
+	var jsonData map[string]interface{}
+	json.Unmarshal([]byte(fields), &jsonData)
+	json := colorizeJson(jsonData, 0)
 
-			color.WhiteString(fmt.Sprintf("\n[ORIGIN ] %s:%d\n", file, line)),
-		)
+	if file != "" {
+		h.l.Println(timeStr, level, msg, h.formatOrigin(json, file, line))
 	} else {
-		h.l.Println(timeStr, level, msg, color.WhiteString(fields))
+		h.l.Println(timeStr, level, msg, fields)
 	}
+}
+
+func (h *PrettyHandler) formatOrigin(json string, file string, line int) string {
+	if len(json) > 0 {
+		return fmt.Sprintf("\n├╴[%s ", json[3:]) + color.WhiteString(fmt.Sprintf("\r└╴[ORIGIN] %s:%d\n", file, line))
+	}
+	return color.WhiteString(fmt.Sprintf("\n└╴[ORIGIN ] %s:%d\n", file, line))
 }
 
 func (h *PrettyHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
 	return &PrettyHandler{
 		Handler: h.Handler,
 		l:       h.l,
-		attrs:   attrs,
+		attrs:   append(h.attrs, attrs...),
+		groups:  h.groups,
 	}
 }
 
@@ -117,5 +185,7 @@ func (h *PrettyHandler) WithGroup(name string) slog.Handler {
 	return &PrettyHandler{
 		Handler: h.Handler.WithGroup(name),
 		l:       h.l,
+		attrs:   h.attrs,
+		groups:  append(h.groups, name),
 	}
 }
